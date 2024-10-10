1066f1578ae5aa47aec9da759264f626899e994f
Alejandro <miperritomini13@gmail.com>
2024-10-10 09:12:21 +0100

    Entrega de archivos

---
+++ formulario.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="informacion.php" method="post">
+        Nombre: <input type="text" name="nombre"><br>
+        Email:  <input type="text" name="email"><br>
+        Edad:   <input type="text" name="edad"><br>
+        País:   <select name="pais" id="pais">
+                    <option value="Espana">España</option>
+                    <option value="Alemania">Alemania</option>
+                    <option value="Suiza">Suiza</option>
+                    <option value="Bulgaria">Bulgaria</option>
+                    <option value="Japon">Japón</option>
+        <input type="hidden" name="form_enviado" value="1">
+        <input type="submit">
+    </form>
+</body>
+</html>
+
+<?php
+
+    session_start();
+    if (isset($_POST['form_enviado']) && $_POST['form_enviado'] == '1'){
+        $nombre=$_POST['nombre'];
+        $email=$_POST['email'];
+        $edad=$_POST['edad'];
+        $pais=$_POST['pais'];
+
+        $nombreVal=false;
+        $emailVal=false;
+        $edadVal=false;
+        $paisVal=false;
+
+        if (!$nombre==""){
+            $nombreVal=true;
+            $_SESSION['nombre']=$nombre;
+            echo $_SESSION['nombre'];
+        }else{
+            echo "Hubo un problema con el nombre<br>";
+        }
+        if (preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/",$email)){
+            $emailVal=true;
+            $_SESSION['email']=$email;
+            echo $email;
+        }else{
+            echo $email;
+            echo "Hubo un problema con el correo<br>";
+        }
+        if ((preg_match("/[0-9]{1,3}$/",$edad))){
+            $edadVal=true;
+            $_SESSION['edad']=$edad;
+            echo $edad;
+        }else{
+            echo "Hubo un problema con la edad<br>";
+        }
+
+        if (isset($pais)){
+            $paisVal=true;
+            $_SESSION['pais']=$pais;
+            echo $pais;
+        }else{
+            echo "Hubo un problema con el país<br>";
+        }
+
+
+    }
---
+++ informacion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Datos del formulario</h1>
+</body>
+</html>
+
+<?php
+
+    session_start();
+
+    echo $_SESSION['nombre'];
+    echo "<br>";
+    echo $_SESSION['email'];
+    echo "<br>";
+    echo $_SESSION['edad'];
+    echo "<br>";
+    echo $_SESSION['pais'];
